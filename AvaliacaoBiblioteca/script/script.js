64735eeeeee0c6490e67eb1dcba8ed75c67ede8d
Den09hub <denyel.1005@gmail.com>
2025-03-25 15:58:14 -0400

    task2

--- AvaliacaoBiblioteca/script/script.js
+++ AvaliacaoBiblioteca/script/script.js
@@ -1,20 +1,2 @@
-// Função para limpar o formulário
-function limparFormulario() {
-    document.getElementById("formCadastro").reset();
-}
-
-// Função para validar o formulário
-function validarFormulario(event) {
-    event.preventDefault(); // Impede o envio imediato do formulário
-
-    // Enviar o formulário via POST
-    document.getElementById("formCadastro").submit();
-
-    // Mostrar o alerta de sucesso
-    alert("Cadastro salvo com sucesso!");
-
-    // Limpar o formulário
-    limparFormulario();
-}
 
 
